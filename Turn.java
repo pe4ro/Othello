38e415d69e5fff080f7dafed053b11b5b1019849
lfmingo <lfmingo@gmail.com>
2014-05-09 21:19:19 +0200

    Update Turn.java
    Javadoc comments add to Turn.java

--- Turn.java
+++ Turn.java
@@ -1,37 +1,49 @@
 package othello.Othello;
+
 /**
- * Created by lfmingo on 01/04/14.
- *
+ * Implementation o a turn class, tha is a value that changes from 0 to 1 or 1 to 0.
  */
 public class Turn {
 
-
+    /**
+     * Inner value 0 or 1.
+     * Just two turns.
+     */
    private int value;
 
+    /**
+     * Number of turns, by default 2.
+     */
+   private int TURNS = 2;
 
+    /**
+     * Generates a random turn
+     */
    public Turn() {
-        this.value =  (int)(Math.random() * 2.0d );
+        this.value =  (int)(Math.random() * TURNS);
    }
 
     /**
-     *according to the rules, black start to play, but we can change it if we want
-     * This class is finished, only is necessary to decide who started first, black, white or random
-      * @param v This parameter adjust the value ...
+     * According to the rules, black start to play, but we can change it if we want
+     * * @param v This parameter adjust the turn value, 0 or 1.
      */
    public Turn(int v) {
+
        this.value = v;
    }
 
-
     /**
-     *
-     * @return return the value of the turn ...
+     * Get the turn value
+     * @return return the value of the turn 0 or 1
      */
     public int getTurn() {
         return this.value;
     }
 
+    /**
+     * Change the turn value from 0 to 1 or from 1 to 0.
+     */
     public void change() {
-        this.value = (this.value + 1) % 2;
+        this.value = (this.value + 1) % TURNS;
     }
 }
