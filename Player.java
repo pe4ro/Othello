3a58975f934de6b21cf8ef9c161e07cdf8f1040b
meankoala <amandine.eap@gmail.com>
2014-05-14 15:37:24 +0100

    Cleanup & Comments

--- Player.java
+++ Player.java
@@ -1,47 +1,85 @@
 package othello.Othello;
+
 import java.io.*;
 import java.util.ArrayList;
-   
-    public class Player{
-    
-    private String name;
-    private int color;
-    private Board board;
 
-    public Player(String name, int color, Board board) {
-        this.name = name;
-        this.color = color;
-        this.board = board;
-    }
-    public void setNames() throws IOException{
-    	BufferedReader line= new BufferedReader(new InputStreamReader (System.in));
-    	System.out.println("tell me your name: ");
-    	this.name=line.readLine();	
-    }
-    public String getName() {
-        return this.name;
-    }
-    
-    public int getColor() {
-        return this.color;
-    }
+/**
+ * Created by pmunoz on 01/04/14. Updated by aeap and jgeorge on 14/05/14.
+ */
 
-    public void placeChip(int row, int col) {
-        this.board.placeChip( this.color, row, col );
-        
-        Move move = new Move(row, col);
-        this.board.replaceChip(move, this.color);
-    }
-    
-    public void findCanSelect(){
+public class Player {
 
-        ArrayList<Move> moves = board.validMove(this.color);
+	private String name; // name of the player
+	private int color; // color of the chips
+	private Board board; // board
 
-        //for( int i=0; i<moves.size(); i++)
-        //    board.setCanSelect(moves.get(i));
+	/**
+	 * Class constructor. Creates a player.
+	 * 
+	 * @param String
+	 *            name - the player's name
+	 * @param int color - the color of the chips
+	 * @param Board
+	 *            board - the board used
+	 */
+	public Player(String name, int color, Board board) {
+		this.name = name;
+		this.color = color;
+		this.board = board;
+	}
 
-        for (Move move : moves)
-            board.setCanSelect(move);
-    }
-    }
+	/**
+	 * Prompts the user for their name and sets it as the name variable.
+	 * 
+	 * Null values throws a message
+	 */
+	public void setNames() throws IOException {
+		BufferedReader line = new BufferedReader(new InputStreamReader(
+				System.in));
+		System.out.println("tell me your name: ");
+		this.name = line.readLine();
+	}
 
+	/**
+	 * Gets the name of the player.
+	 * 
+	 * @return name of the player
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Gets the number of the player.
+	 * 
+	 * @return the number of the player
+	 */
+	public int getColor() {
+		return this.color;
+	}
+
+	/**
+	 * Places a chip for a chosen player at the chosen location. Replaces the
+	 * chips in between the player's chips to the player's color.
+	 * 
+	 * @param int row - the row of the chip
+	 * @param int col - the column of the chip
+	 */
+	public void placeChip(int row, int col) {
+		this.board.placeChip(this.color, row, col);
+
+		Move move = new Move(row, col);
+		this.board.replaceChip(move, this.color);
+	}
+
+	/**
+	 * Find the valid moves and allow them to be selected.
+	 */
+	public void findCanSelect() {
+
+		ArrayList<Move> moves = board.validMove(this.color);
+
+		for (Move move : moves)
+			board.setCanSelect(move);
+	}
+}
