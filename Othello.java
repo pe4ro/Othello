4df017f590a90e9573771701477de78acad42f22
pe4ro <pedro.munoz.garcia@alumnos.upm.es>
2014-05-14 09:18:31 +0100

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -16,19 +16,34 @@ public class Othello {
 
     
 
-    public void startGame() {
+    public void startGame() throws IOException /* the game starts*/{
 
         int who = this.initPlayers();
-        this.turn = new Turn(who+1);
-        System.out.println("Black chips start");
+        this.turn = new Turn((who+1) % 2);
+        for(int i=0;i<2;i++){
+        System.out.print ("Player"+ (i+1)+ " ");
+        players[i].setNames();
+        }
+        System.out.println( players[0].getName() +" moves");
         this.players[ turn.getTurn() ].findCanSelect();
         board.display();
 
         while(!board.gameOver()) {
 
             // put the row and the col (row, col)
-                
-            int row = this.readRow();
+        	int count = 0;
+        	for (int j = 0; j < Board.NUM; j++)
+				for (int i = 0; i < Board.NUM; i++) 
+					if (board.findLegalMoveNew(new Move(i, j), turn.getTurn()) == true) {
+						count++;
+					}    
+					
+			if(count==0){
+				turn.change();
+				board.display();
+				count=0;
+			}
+			else{ int row = this.readRow();
             int col = this.readCol();
            /// empty
            /// I can put
@@ -37,15 +52,17 @@ public class Othello {
                 this.players[ turn.getTurn() ].placeChip( row, col);
                 turn.change();
             }
-            //tHATS NEW
-            if(turn.getTurn()==0)
-            	System.out.println("White player moves(Chip O)");
-            else System.out.println("Black player moves(Chip X)");
+            //that is new
+            
+            //if(turn.getTurn()==0)
+            //	System.out.println("Black player moves(Chip X)");
+            //else System.out.println("White player moves(Chip O)");
             // put chip
             // change turn
             this.players[ turn.getTurn() ].findCanSelect();
             board.display();
-
+            System.out.println( players[turn.getTurn()].getName() +" moves");
+			}
         }
     }
 
@@ -76,7 +93,7 @@ public class Othello {
         return value;
     }
 
-    private int initPlayers() {
+    private int initPlayers()/* Create 2 players*/ {
         Turn aux = new Turn();
         this.players[0] = new Player("name 1", aux.getTurn(), this.board);
         aux.change();
@@ -86,7 +103,7 @@ public class Othello {
         else return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         new Othello().startGame();
 
