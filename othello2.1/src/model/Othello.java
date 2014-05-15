1946a36d8c974fb2834323579a82da112b41f137
meankoala <amandine.eap@gmail.com>
2014-05-15 14:26:44 +0100

    Updated format.

--- othello2.1/src/model/Othello.java
+++ othello2.1/src/model/Othello.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Created by pmunoz on 01/04/14. Updated by zwodnik on 09/05/14. Updated by
+ * Created by pmunoz on 01/04/14. Updated by zwodnik on 09/05/14.
+ * Updated by
  * aeap and jgeorge on 14/05/14.
  */
 
 public class Othello {
 
 	private Board board = new Board(); // creates the board
-	private Player[] players = new Player[2]; // array of players for two
-												// players
+	private Player[] players = new Player[2]; // array of players for two players
 	private Turn turn; // creates a turn
 
 	/**
@@ -29,72 +29,46 @@ public class Othello {
 	 * 
 	 * @throws java.io.IOException
 	 */
-	public void startGame() throws IOException /* the game starts */{
+	public void startGame() throws IOException{  /* the game starts */
 
 		int who = this.initPlayers(); // initializes the first player
 		this.turn = new Turn((who + 1) % 2); // initializes the turn
 
-		for (int i = 0; i < 2; i++) { // prompts both the players for their
-										// names
+		for (int i = 0; i < 2; i++) { // prompts both the players for their names
 			System.out.print("Player" + (i + 1) + " ");
 			players[i].setNames();
 		}
 
-		System.out.println(players[0].getName() + " moves"); // indicates who
-																// has to play,
-																// starting with
-																// the player
-																// whose turn it
-																// is
-		this.players[turn.getTurn()].findCanSelect(); // finds the possible
-														// moves for the player
-														// playing
+		System.out.println(players[0].getName() + " moves"); // indicates who has to play, starting with the player whose turn it is
+		this.players[turn.getTurn()].findCanSelect(); // finds the possible moves for the player playing
 		board.display(); // displays board
 
-		while (!board.gameOver()) { // when not GameOver, find the possible
-									// moves from the player
-
+		while (!board.gameOver()) { // when not GameOver, find the possible moves from the player
 			int count = 0; // count of possible moves
-			for (int j = 0; j < Board.NUM; j++)
-				// search the entire board
-				for (int i = 0; i < Board.NUM; i++)
+                        
+			for (int j = 0; j < Board.NUM; j++){ // search the entire board
+				
+				for (int i = 0; i < Board.NUM; i++){
 					if (board.findLegalMoveNew(new Move(i, j), turn.getTurn()) == true) {
-						count++; // add a possible move to the count
-					}
-			if (count == 0) { // when no possible moves
-				turn.change(); // change the turn to the other player
-				board.display(); // display the updated board
-				count = 0; // reset count to 0
-			}
-
-			else {
-				int row = this.readRow(); // prompts the player for the row
-											// wanted
-				int col = this.readCol(); // prompts the player for the column
-											// wanted
-
-				Move move = new Move(row, col); // creates a new move
-				if (board.canSelect(move)) { // if move valid
-					this.players[turn.getTurn()].placeChip(row, col); // place
-																		// the
-																		// chip
-					turn.change(); // change the turn to the other player
-				}
-
-				this.players[turn.getTurn()].findCanSelect(); // find the
-																// possible
-																// moves at the
-																// location
+                                            int row = this.readRow(); // prompts the player for the row wanted
+                                            int col = this.readCol(); // prompts the player for the column wanted
+                                            count++; // add a possible move to the count
+                                            Move move = new Move(row, col); // creates a new move
+                                            this.players[turn.getTurn()].placeChip(row, col); // place the chips     
+					} // end if
+                                        
+                                        else { // when no possible moves
+                                            count = 0; // reset count to 0
+                                        }
+                                } // end for loop
+                                
+                                turn.change(); // change the turn to the other player											
+				this.players[turn.getTurn()].findCanSelect(); // find the possible															// moves at the
 				board.display(); // display updated board with possible moves
-				System.out
-						.println(players[turn.getTurn()].getName() + " moves"); // indicates
-																				// who
-																				// has
-																				// to
-																				// play
-
-			}
-		}
+				System.out.println(players[turn.getTurn()].getName() + " moves"); // indicates who has to play
+																				
+			} // end for loop
+		} // end while loop
 	}
 
 	/**
@@ -105,11 +79,9 @@ public class Othello {
 	private int initPlayers() {
 		Turn aux = new Turn(); // temporary turn
 
-		this.players[0] = new Player("name 1", aux.getTurn(), this.board); // player
-																			// 1
+		this.players[0] = new Player("name 1", aux.getTurn(), this.board); // player 1
 		aux.change(); // changes to player 2
-		this.players[1] = new Player("name 2", aux.getTurn(), this.board); // player
-																			// 2
+		this.players[1] = new Player("name 2", aux.getTurn(), this.board); // player 2
 
 		if (aux.getTurn() == 0) { // if player is player 1 then start with black
 			return 1;
@@ -124,14 +96,9 @@ public class Othello {
 	 * @return the value of the row wanted
 	 */
 	private int readRow() {
-		System.out.print("Select a row: "); // Displays a message asking to
-											// select a row
+		System.out.print("Select a row: "); // Displays a message asking to select a row
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // prompts
-																					// for
-																					// input
-																					// of
-																					// row
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // prompts for input of a row
 		Integer value = -1;
 		try {
 			value = Integer.parseInt(br.readLine()); // reads the value selected
@@ -146,14 +113,8 @@ public class Othello {
 	 * @return the value of the column wanted
 	 */
 	private int readCol() {
-		System.out.print("Select a column: "); // Displays a message asking to
-												// select a column
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // prompts
-																					// for
-																					// input
-																					// of
-																					// column
+		System.out.print("Select a column: "); // Displays a message asking to select a column
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // prompts for input of column
 		Integer value = -1;
 		try {
 			value = Integer.parseInt(br.readLine()); // reads the value selected
